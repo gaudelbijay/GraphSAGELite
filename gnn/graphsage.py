a8316ab06815f1803b3253cab966281b5f635529
gaudelbijay <gaudelbijay10@gmail.com>
2020-05-26 21:23:04 +0545

    graphsage model

--- gnn/graphsage.py
+++ gnn/graphsage.py
@@ -1,64 +1,98 @@
-import tensorflow as tf 
-import numpy as np 
-from tensorflow.keras.initializers import glorot_uniform,zeros
-from tensorflow.keras.layers import Input,Dense,Dropout,Layer,LSTM
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.initializers import glorot_uniform, Zeros
+from tensorflow.keras.layers import Input, Dense, Dropout, Layer, LSTM
 from tensorflow.keras.regularizers import l2
+from tensorflow.keras import Model
 
 
-def GraphSAGE(feature_dim,neighbor_num,n_hidden,n_classes,use_bias=True,
-				activation=tf.nn.relu,aggregator_type='mean',dropout_rate=0.0,l2_reg=0):
+def GraphSAGE(feature_dim, neighbor_num, n_hidden, n_classes, use_bias=True, activation=tf.nn.relu,
+              aggregator_type='mean', dropout_rate=0.0, l2_reg=0):
+    features = Input(shape=(feature_dim,))
+    node_input = Input(shape=(1,), dtype=tf.int32)
+    neighbor_input = [Input(shape=(l,), dtype=tf.int64) for l in neighbor_num]
+    # print('features: ', features)
 
-	features = Input(shape=(features_dim,))
-	node_input = Input(shape=(1,),dtype=tf.int32)
-	neighbor_input = [Input(shape=(1,),dtype=tf.int32) for n in neighbor_num]
-	if aggregator_type == 'mean':
-		aggregator = MeanAggregator
-	else: aggregator = PoolingAggregator
+    if aggregator_type == 'mean':
+        aggregator = MeanAggregator
+    else:
+        aggregator = PoolingAggregator
 
-	h = features 
-	for i in range(len(neighbor_num)):
-		if i>0:
-			features_dim = n_hidden
-		if i == len(neighbor_num)-1:
-			activation = tf.nn.softmax
-			n_hidden = n_classes
-		h = aggregator(units = n_hidden,input_dim=features_dim,activation=activation,
-			l2_reg=l2_reg,use_bias=use_bias,dropout_rate=dropout_rate,neigh_max=neighbor_num[i],
-			aggregator=aggregator_type)[h,node_input,neighbor_input[i]]
-	output = h 
-	input_list = [features,node_input]+neighbor_input
-	model = Model(input_list,outputs=output)
+    h = features
+    for i in range(0, len(neighbor_num)):
+        if i > 0:
+            feature_dim = n_hidden
+        if i == len(neighbor_num) - 1:
+            activation = tf.nn.softmax
+            n_hidden = n_classes
+        h = aggregator(units=n_hidden, input_dim=feature_dim, activation=activation, l2_reg=l2_reg, use_bias=use_bias,
+                       dropout_rate=dropout_rate, neigh_max=neighbor_num[i], aggregator=aggregator_type)(
+            [h, node_input, neighbor_input[i]])  #
 
+    output = h
+    input_list = [features, node_input] + neighbor_input
+    model = Model(input_list, outputs=output)
+    return model
 
 
 class MeanAggregator(Layer):
- 	"""docstring for MeanAggregator"""
- 	def __init__(self, units,input_dim,activation=tf.nn.relu,neigh_max,concat=True,dropout_rate=0.0,
- 					l2_reg=0,use_bias=False,seed=1024,aggregator=aggregator_type,**kwargs):
- 		super(MeanAggregator, self).__init__(**kwargs)
- 		self.units = units 
- 		self.neigh_max=neigh_max
- 		self.concat = concat 
- 		self.dropout_rate = dropout_rate
- 		self.l2_reg = l2_reg
- 		self.use_bias = use_bias 
- 		self.activation = activation 
- 		self.seed = seed 
- 		self.input_dim = input_dim 
+    """docstring for MeanAggregator"""
 
- 	def build(self,input_shape):
- 		pass 
- 		 
+    def __init__(self, units, input_dim, neigh_max, activation=tf.nn.relu, concat=True, dropout_rate=0.0,
+                 l2_reg=0, use_bias=False, seed=1024, aggregator='mean', **kwargs):
+        super(MeanAggregator, self).__init__(**kwargs)
+        self.units = units
+        self.neigh_max = neigh_max
+        self.concat = concat
+        self.dropout_rate = dropout_rate
+        self.l2_reg = l2_reg
+        self.use_bias = use_bias
+        self.activation = activation
+        self.seed = seed
+        self.input_dim = input_dim
+
+    def build(self, input_shapes):
+
+        self.neigh_weights = self.add_weight(shape=(self.input_dim, self.units),
+                                             initializer=glorot_uniform(
+                                                 seed=self.seed),
+                                             regularizer=l2(self.l2_reg),
+                                             name="neigh_weights")
+        if self.use_bias:
+            self.bias = self.add_weight(shape=self.units, initializer=Zeros(),
+                                        name='bias_weight')
+
+        self.dropout = Dropout(self.dropout_rate)
+        self.built = True
+
+    def call(self, inputs, training=None):
+        features, node, neighbours = inputs
+
+        node_feat = tf.nn.embedding_lookup(features, node)
+        neigh_feat = tf.nn.embedding_lookup(features, neighbours)
+
+        node_feat = self.dropout(node_feat, training=training)
+        neigh_feat = self.dropout(neigh_feat, training=training)
+
+        concat_feat = tf.concat([neigh_feat, node_feat], axis=1)
+        concat_mean = tf.reduce_mean(concat_feat, axis=1, keepdims=False)
+
+        output = tf.matmul(concat_mean, self.neigh_weights)
+
+        if self.use_bias:
+            output += self.bias
+        if self.activation:
+            output = self.activation(output)
+
+        # output = tf.nn.l2_normalize(output, dim=-1)
+        output._uses_learning_phase = True
+
+        return output
+
 
 class PoolingAggregator(Layer):
-	"""docstring for PoolingAggregator"""
-	def __init__(self, arg):
-		super(PoolingAggregator, self).__init__()
-		self.arg = arg
-		
-		
+    """docstring for PoolingAggregator"""
 
-	
-
-
-
+    def __init__(self, arg):
+        super(PoolingAggregator, self).__init__()
+        self.arg = arg
